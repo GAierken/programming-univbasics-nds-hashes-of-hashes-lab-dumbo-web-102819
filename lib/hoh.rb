1d26a6a1fd54df4f8f17af4c8a04644b40f4bb6c
Guligena Aierken <guligena_aierken@163.com>
2019-10-24 02:00:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -6,7 +6,8 @@
 #
  HoH= {
    :PORTION_1 => {label: "Kingdom",sub_category: {label: "Phylum",sub_category: {label: "Class"}}},
-   :PORTION_2 => {label: "Order"},:PORTION_3 => {label: "Family",sub_category: {label: "Genus"}},
+   :PORTION_2 => {label: "Order"},
+   :PORTION_3 => {label: "Family",sub_category: {label: "Genus"}},
    :PORTION_4 => {label: "Species",sub_category: nil}
  }
 
